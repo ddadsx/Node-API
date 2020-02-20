2981c2f3bcb86fd322977edcd02e435e48c602ee
Douglas de Assis da Silva <ddadsx@gmail.com>
2020-02-19 23:49:09 -0400

    [ADD] Product API in Node.js

---
+++ README.md
@@ -0,0 +1,5 @@
+npm start
+
+https://app.balta.io/courses/1972
+
+https://cloud.mongodb.com/v2/5e41d3b9014b76a14ab90a5c#metrics/replicaSet/5e41d5108598c6bf0f7c8499/explorer/test/products/find
---
+++ src/app.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const router = express.Router();
+
+// Conecta ao banco
+mongoose.connect(process.env.DB_HOST,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    });
+
+// Carrega os Models
+const Product = require('./models/product');
+
+// Carrega as rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+module.exports = app;
